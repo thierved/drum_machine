07360db2a4b963ca37af91f6c6aba6508a6813e2
Thierno Sadou Diallo <thierved01@gmail.com>
2018-08-14 22:43:38 +0000

    refactored some code

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
+function playSound(props)  {
+    const audio = document.getElementById(props.text);
+    if (props.isOn === true) {
+        props.tune(props.name);            
+        audio.play();
+    }   
+}
 
 const DrumPad = (props) => {  
 
     document.addEventListener('keypress', (event) => {
         if (event.key === props.text.toLowerCase()) {
-            playSound(props.name);
+            playSound(props);
         }
-    });
-    
-    function playSound(name)  {
-        const audio = document.getElementById(props.text);
-        if (props.isOn === true) {
-            props.tune(name);            
-            audio.play();
-        }   
-    }
+    });  
+
 
     return(
-        <div className='drum-pad' id={props.name} onClick={() => playSound(props.name)} >
+        <div className='drum-pad' id={props.name} onClick={playSound} >
             {props.text}
             <audio src={props.src} className='clip' id={props.text} type='audio/mp3'/>
         </div>
     );
-
     
 };
 
