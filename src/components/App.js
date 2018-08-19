0c0bfa6e1dd7493e3362bfca92a9bd2145e660ef
Thierno Sadou Diallo <thierved01@gmail.com>
2018-08-19 16:52:20 +0000

    finish added the functionality of the app

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,12 @@ import DrumPads from './DrumPads';
 import '../App.css';
 import Controlls from './Controlls';
 
+
+
+
+
 const App = () => {
-  
+
   return (
     <div id="drum-machine">
       <Controlls />
