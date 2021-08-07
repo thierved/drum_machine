d844e1993e3d787625ab90a46d9611ac0ca85f7e
thierno sadou diallo <thierved01@gmail.com>
2021-08-07 19:26:19 +0000

    Inital app setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/App';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
+  <React.StrictMode>
+    <h1>Drum Machine</h1>
     <App />
-    , document.getElementById('root')
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
