4d35d941916c49a9215d46155f86cd87dcac6f88
thierno sadou diallo <thierved01@gmail.com>
2021-08-25 13:54:54 +0000

    fixed some errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import { useState } from 'react';
-import Display from './components/Display';
-import Keypad from './components/Keypad';
+import React, { useState, useEffect } from 'react';
 import data from './data';
 
+const Display = ({ soundName, switchBtn, setSwitchBtn }) => {
+
+  return (
+    <div id="display">
+      <button id="switch"
+        onClick={() => { setSwitchBtn(!switchBtn) }}
+      >ON | OFF</button>
+      <div id="screen">{soundName && switchBtn ? `${soundName}` : ""}</div>
+    </div>
+  );
+}
+
+const Pad = ({ item, playSound, setSoundName }) => {
+  return (
+    <button
+      className="drum-pad"
+      id={item.name}
+      onClick={() => {
+        playSound(item.pad);
+        setSoundName(item.name);
+      }}
+    >
+      <audio
+        src={item.sound}
+        className="clip"
+        id={item.pad}
+      ></audio>
+      {item.pad}
+    </button>
+  );
+}
+
+const Keypad = ({ soundData, switchBtn, setSoundName, playSound }) => {
+
+  return (
+    <div className="keypad">
+      {soundData.map(item => {
+        return <Pad key={item.pad}
+          item={item}
+          switchBtn={switchBtn}
+          setSoundName={setSoundName}
+          playSound={playSound} />
+      })
+      }
+    </div>
+  );
+}
 
 const App = () => {
   const soundData = data;
   const [switchBtn, setSwitchBtn] = useState(true);
   const [soundName, setSoundName] = useState();
 
+  const playSound = (key) => {
+    if (switchBtn) {
+      document.getElementById(key.toUpperCase()).play();
+      data.forEach(d => {
+        if (d.pad === key.toUpperCase()) {
+          setSoundName(d.name.replaceAll("_", " ").toUpperCase());
+        }
+      })
+    }
+  }
+
+  const handleKeyPress = e => {
+    if ("QWEASDZXC".includes(e.key.toUpperCase())) {
+      playSound(e.key);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keypress", e => handleKeyPress(e));
+  });
+
   return (
     <div id="drum-machine">
       <Keypad soundData={soundData}
-        switchBtn={switchBtn} 
-        setSoundName={setSoundName}/>
+        switchBtn={switchBtn}
+        setSoundName={setSoundName}
+        playSound={playSound} />
       <Display
         setSwitchBtn={setSwitchBtn}
-        switchBtn={switchBtn} 
-        soundName={soundName}/>
+        switchBtn={switchBtn}
+        soundName={soundName} />
     </div>
   );
 }
